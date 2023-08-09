9a3dcf78bc689a424e2f3698d454d922fab04186
Dappmaster (FullstackBlockchain Developer) <60449971+Full-stack-Web-Dev-BD@users.noreply.github.com>
2023-08-09 12:20:52 +0600

    drag in  progress

--- src/pages/dashboard/JobsZone/Upload/FileUpload.jsx
+++ src/pages/dashboard/JobsZone/Upload/FileUpload.jsx
@@ -1,14 +1,60 @@
-import React from "react"; 
+import React, { useState } from "react";
+
 const FileUpload = () => {
+  const [isDragging, setIsDragging] = useState(false);
+
+  const handleFileSelect = () => {
+    // ... (same as before)
+  };
+
+  const handleFileUpload = (event) => {
+    // ... (same as before)
+  };
+
+  const handleDragEnter = (event) => {
+    event.preventDefault();
+    setIsDragging(true);
+  };
+
+  const handleDragLeave = () => {
+    // setIsDragging(false);
+  };
+
+  const handleDrop = (event) => {
+    event.preventDefault();
+    setIsDragging(false);
+    // Handle dropped file here
+    const droppedFile = event.dataTransfer.files[0];
+    console.log("Dropped file:", droppedFile);
+  };
+
   return (
+    <div id="file_upload_box">
     <div className="container">
       <h3 className="text-center mt-5">Upload your CAD Files</h3>
-      <div className="text-center file_upload_box mt-4 mb-4">
-        <div className="mt-4">
-          <img src={require('./upload.png')} className="mb-4" />
-          <h4>
-            Drag-and-drop your 3D Printing files anywhere, or browse files
-          </h4>
+      <div
+        className={`text-center file_upload_box mt-4 mb-4 ${isDragging ? "dragging" : ""}`}
+        onDragEnter={handleDragEnter}
+        onDragOver={(event) => event.preventDefault()}
+        onDragLeave={handleDragLeave}
+        onDrop={handleDrop}
+      >
+        <div
+          className={`mt-4 ${isDragging ? "dragging" : ""}`}
+          onDragEnter={handleDragEnter}
+          onDragOver={(event) => event.preventDefault()}
+          onDragLeave={handleDragLeave}
+          onDrop={handleDrop}
+        >
+          <img
+            id="uploadImage"
+            src={require('./upload.png')}
+            className={`mb-4 ${isDragging ? "dragging" : ""}`}
+            onClick={handleFileSelect}
+            style={{ cursor: "pointer" }}
+            alt="Upload"
+          />
+          <h4>Drag-and-drop your 3D Printing files anywhere, or browse files</h4>
           <p>
             We accept the following CAD files for 3D printing: MESH (.stl), STEP
             (.stp/.step), SOLIDWORKS (.sldprt), or IGES (.igs/.iges).
@@ -19,6 +65,7 @@ const FileUpload = () => {
       <div className="text-center">
         <button className="btn btn-cont common_button">Continue</button>
       </div>
+    </div>
     </div>
   );
 };
